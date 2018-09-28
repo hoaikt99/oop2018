0a3743735396c3d27df3f8a4e3278fe6b6a465ae
hoaikt99 <43249738+hoaikt99@users.noreply.github.com>
2018-09-28 11:12:04 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -3,12 +3,30 @@ package week2.task1;
 public class Task1 {
 
     public static int gcd(int a, int b) {
-        // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+         for(int i=0;a!=b;i++)
+	   {
+		if(a>b) a=a-b;
+		else b=b-a;
+	   }
+
+	   return a;
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+      int ar [] = new int [n];
+        ar[0]=0;
+        ar[1]=1;
+        if(n==0) return ar[0];
+	if(n==1) return ar[1];
+	else
+        {
+            for(int i=2; i<=n-1; i++)
+            {
+            ar[i]=ar[i-1]+ar[i-2];
+            }
+            int m=ar[n-1];
+            return m;
+	}
     }
 }
