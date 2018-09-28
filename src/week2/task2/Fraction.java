8f58ef599994c3f461c599a577ed0b2d54c2ed6d
hoaikt99 <43249738+hoaikt99@users.noreply.github.com>
2018-09-28 15:51:34 +0700

    Update Fraction.java

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -3,33 +3,28 @@ package week2.task2;
 public class Fraction {
 
     // TODO: khai báo các thuộc tính
-    private int numerator, denominator;
+   private int numerator, denominator;
     public Fraction(int numerator, int denominator) {
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
-    int a=Math.abs(numerator), b=Math.abs(denominator);
-    if(a==0&&b!=0)  a=b;
-    else if(b==0&&a!=0) b=a;
-    else {
-        while (a != b) {
-            if (a > b) a = a - b;
-            else b = b - a;
-        }
-    }
+        int a=numerator;
+        int b=denominator;
+        a=Math.abs(a);
+	    b=Math.abs(b);
+        
+	    while (a!=b){
+		    if(a>b) a=a-b;
+		    else if(a<b) b=b-a;
+	    }
         this.numerator=numerator/a;
         this.denominator=denominator/a;
-    
     }
-
     public Fraction add(Fraction other) {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        
         Fraction sum = new Fraction(this.numerator*other.denominator+other.numerator*this.denominator,this.denominator*other.denominator);
         return sum;
     }
-
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
-        
         Fraction hieu = new Fraction(this.numerator*other.denominator-other.numerator*this.denominator,this.denominator*other.denominator);
         return hieu;
     }
@@ -44,5 +39,10 @@ public class Fraction {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
         Fraction chia = new Fraction(this.numerator*other.denominator,this.denominator*other.numerator);
         return chia;
+    }
+    public boolean equals(Object obj){
+        Fraction other = (Fraction)obj;
+        if(this.numerator*other.denominator-other.numerator*this.denominator==0) return true;
+        else return false;
     }
 }
