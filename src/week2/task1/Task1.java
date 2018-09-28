4b8a03fc016c7db18daa4f70f2a5aa0ee3b9243e
hoaikt99 <43249738+hoaikt99@users.noreply.github.com>
2018-09-28 13:50:49 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -3,13 +3,17 @@ package week2.task1;
 public class Task1 {
 
     public static int gcd(int a, int b) {
-         for(int i=0;a!=b;i++)
+        if( a!=0 && b!=0)
+	{
+		for(int i=0;a!=b;i++)
 	   {
 		if(a>b) a=a-b;
 		else b=b-a;
 	   }
 
 	   return a;
+	}
+	    else return 0;
     }
 
     public static int fibonacci(int n) {
@@ -19,7 +23,7 @@ public class Task1 {
         ar[1]=1;
         if(n==0) return ar[0];
 	if(n==1) return ar[1];
-	else
+	if(n>=2)
         {
             for(int i=2; i<=n-1; i++)
             {
@@ -28,5 +32,7 @@ public class Task1 {
             int m=ar[n-1];
             return m;
 	}
+	    else 
+		return 0;
     }
 }
